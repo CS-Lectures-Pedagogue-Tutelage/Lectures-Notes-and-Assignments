0d2aeb4061638c8d3c4dd25891dced84704983df
Karan <karan.tamboli2001@gmail.com>
2022-05-16 20:52:56 +0530

    Folders of Assignments and Notes

---
+++ Lecture 2/Pedagogue_Tutelage_Lec_2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+// C++ IS A MULTI-PARADIGM LANGUAGE. THAT MEANS IT CAN BE USEFUL FOR BOTH FUNCTIONAL PROGRAMMING
+//      AND OBJECT-ORIENTED PROGRAMMING.
+
+// C++ IS CASE SENSITIVE LANGUAGE.
+// C++ IS STATISTICALLY TYPED LANGUAGE.
+
+// DATA TYPES :- A DATA TYPE IS SIMPLY A CLASSIFICATION OF DATA WE INTENDED TO USE
+// IT CARRIES OUT 2 DIFFERENT WORK.
+//      1) TO TELL A VARIABLE WHAT TYPE OF DATA IT IS GOIN TO STORE.
+//      2) TO TELL THE COMPILER HOW MUCH SPACE/SIZE TO BE ALLOCATED.
+
+//                                      DATA TYPES
+//            PRIMITIVE DATA TYPE                        NON-PRIMITIVE DATA TYPE
+//               * INTEGER (int)                            /               \
+//               * CHARACTER (char)                DERIVED DATA TYPE        USER DEFINDED
+//               * BOOLEAN (bool)                   * ARRAYS                  * CLASS
+//               * FLOATING POINT (float)           * FUNCTIONS               * STRUCTURE
+//               * DOUBLE FLOATING POINT (double)   * POINTERS                * UNION
+//                                                                            * ENUMERATION
+
+//  ALL THINGS MENTIONED ABOVE INSIDE () ARE KEYWORDS USE TO CREATE A VARIABLE OF THAT DATA TYPE.
+
+//  sizeof() -- IT IS SPECIAL OPERATOR IN C++ WHICH TELLS US HOW MUCH MEMORY HAS BEEN OCCUPIED BY A VARIABLE.
+//  FOR EXAMPLE --- sizeof(int) <---- will give me output as 4 (4 bytes)
+//                  sizeof(double) <---- will give me output as 8 (8 bytes)
+
+//  STATISTICALLY TYPED LANGUAGE & DYNAMICALLY TYPED LANGUAGE -- THE BASIC UNDERSTANDING
+//          In Statically typed language data type of variable's are static, meaning once you set a variable to a type, you cannot change it.
+//              FOR EXAMPLE - string var_name = "Hello";
+//                             ...
+//                             ...
+//                             ...
+//                             var_name = 5     <---- This will generate an error in statistically type language like C++.
+
+//          In Dynamically typed language variables' types are dynamic, meaning after you set a variable to a type, you CAN change it.
+//              FOR EXAMPLE - var name = "Hello";
+//                             ...
+//                             ...
+//                             name = 5         <--- Perfectly alright in langauge like JavaScript
+//
+//          The main advantage with statistically typed language is that all kinds of checking can be done by the compiler, and therefore a lot of trivial bugs are caught at a very early stage.
+
+int main()
+{
+
+    __ marks = 87;
+    Size of marks = ___
+
+        ___ todaysTemp = 37.45f;
+    Size of todaysTemp = ___
+
+        ___ favBrand = "Zoom";
+    Size of fav = ___
+
+        ___ dollarToBitcoin = 0.000034;
+    Size of dollarToBitcoin = ___
+
+        ___ letter = "S";
+    Size of letter = ___
+
+        ___ isUserAuthenticated = true;
+    Size of isUserAuthenticated = ___
+
+        ___ db = 4.00023;
+    Size of db = ___
+
+        ___ total = 496.80f;
+    Size of total = ___
+
+        ___ yourAnswer = 'y';
+    Size of yourAnswer = ___
+
+        ___ password = "nrsj@79hsg";
+    Size of password = ___
+
+        return 0;
+}
